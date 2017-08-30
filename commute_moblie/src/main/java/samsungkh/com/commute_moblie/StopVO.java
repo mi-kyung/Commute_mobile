b8bb39e79a62488fd90b03cf50f6819a815787d9
mi-kyung <31283076+mi-kyung@users.noreply.github.com>
2017-08-30 21:43:37 +0900

    Update StopVO.java

--- commute_moblie/src/main/java/samsungkh/com/commute_moblie/StopVO.java
+++ commute_moblie/src/main/java/samsungkh/com/commute_moblie/StopVO.java
@@ -4,7 +4,8 @@ package samsungkh.com.commute_moblie;
  * Created by Jungyong on 2017-08-27.
  */
 
-public class StopVO {
+public class StopVO implements Serializable
+{
 
     String rt_id;     // route_id
     String stop_id;   // 정거장 id
